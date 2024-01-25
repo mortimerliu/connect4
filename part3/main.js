110625cb96d49cfdafc28c251087d11f695922a1
Hongru Liu <hl3148@columbia.edu>
2024-01-25 00:52:45 -0500

    Configure WebSocket server address.

--- part3/main.js
+++ part3/main.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const board = document.querySelector(".board");
     createBoard(board);
     // Open the WebSocket connection and register event handlers.
-    const websocket = new WebSocket("ws://localhost:8001/");
+    const websocket = new WebSocket(getWebSocketServer());
     initGame(websocket);
     receiveMoves(board, websocket);
     sendMoves(board, websocket);
@@ -80,4 +80,14 @@ function initGame(websocket) {
         }
         websocket.send(JSON.stringify(event));
     });
+}
+
+function getWebSocketServer() {
+    if (window.location.host === "mortimerliu.github.io") {
+        return "wss://connect4-websocket.herokuapp.com/";
+    } else if (window.location.host === "localhost:8000") {
+        return "ws://localhost:8001/";
+    } else {
+        throw new Error(`Unsupported host: ${window.location.host}`);
+    }
 }
